658d213551ee6ab0b468bc97c3637f9a16485d22
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-03-30 21:43:42 -0400

    successful setup of nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
 "use strict";
 
+// website data
+const data = {
+    navlinks: [
+            {url: "index.html", text: "Home"},
+            {url: "storage/resume-2019.pdf", text: "Resume"},
+            {url: "portfolio.html", text: "Portfolio"},
+            {url: "https://github.com/mattConn/", text: "GitHub"},
+            {url: "https://codepen.io/mattConn/", text: "CodePen"},
+            {url: "https://www.linkedin.com/in/mattconndev/", text: "LinkedIn"},
+            {url: "https://twitter.com/mattconndev", text: "Twitter"},
+            {url: "https://dev.to/mattconn", text: "Blog"}
+        ]
+};
+
 const dist = "dist"; // distributable
 const src = "src"; // source
 
 const del = require("del");
 const gulp = require("gulp");
-const pug = require("gulp-pug");
+const nunjucks = require("gulp-nunjucks");
 
 // generate html from pug
 function html() {
-    return gulp.src("pug/*.pug")
-    .pipe(pug())
+    return gulp.src(["src/*.html","!src/_*.html"])
+    .pipe(nunjucks.compile(data))
     .pipe(gulp.dest(`${dist}/`));
 }
 
